031941689cd30e0c7fe842d9746f5aafa4dae9cd
Erik Sesselmann <erik.sesselmann@gmail.com>
2021-11-23 19:38:19 +1100

    deleting junk files

--- zip_project/zip_project/catagory_count_extention.py
+++ zip_project/zip_project/catagory_count_extention.py
@@ -24,52 +24,14 @@ class catagory_counter:
 
         # instantiate the extension object
         ext = cls(item_count)
-
-        # connect the extension object to signals
-        #crawler.signals.connect(ext.spider_opened, signal=signals.spider_opened)
-        #crawler.signals.connect(ext.spider_closed, signal=signals.spider_closed)
-
-        print(dir(crawler))
-        print(dir(signals))
-
         crawler.signals.connect(ext.item_scraped, signal=signals.item_scraped)
         
-
         # return the extension object
         return ext
 
-    def spider_opened(self, spider):
-        logger.info("opened spider %s", spider.name)
-        logger.info("HEHEHEHEHEHEHEHEHEHEHEHEHEHEH")
-
-    def spider_closed(self, spider):
-        logger.info("closed spider %s", spider.name)
-        logger.info("HEHEHEHEHEHEHEHEHEHEHEHEHEHEH")
-
     def item_scraped(self, item, spider):
         self.items_scraped += 1
-        #yield scrapy.Request('https://theurge.com/men/search/?page=3&cat=shoes-boots',callback = self.extension_test_parse)
-        #print(dir(boots))
-        #print(dir(boots.body))
-        #print(boots.body)
-
-       # boots_count = int(boots.xpath('//*[@class="_3-VCf"]/text()').re('^\d{0,10}\+')[0].replace('+',''))
-
-        #print('TOTAL BOOTS', boots_count)
-
-        #print(spider.make_requests_from_url('https://theurge.com/men/search/?page=3&cat=shoes-boots'))
-
+     
         if self.items_scraped%self.catagory_count_log == 0:
         	for k in spider.catagory_count.keys():
         		logger.info(k+' : '+str(spider.catagory_count[k]))
-
-    def extension_test_parse(self,response):
-        print('KEKEKEKEKEKEKEKE')
-        print(response)
-        boot_count = int(response.xpath('//*[@class="_3-VCf"]/text()').re('^\d{0,10}\+')[0].replace('+',''))
-        print(boot_count)
-        return boot_count
-
-
-
-
